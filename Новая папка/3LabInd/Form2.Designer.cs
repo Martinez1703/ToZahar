17341018d0e15988684a10478c2aeb2f564669c4
Martinez1703 <164842551+Martinez1703@users.noreply.github.com>
2025-04-16 13:15:18 +0300

    Add files via upload

---
+++ Новая папка/3LabInd/Form2.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace _3LabInd
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnIncrease = new System.Windows.Forms.Button();
+            this.BtnDecrease = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 375);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Нарисовать фигуры";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // BtnIncrease
+            // 
+            this.BtnIncrease.Location = new System.Drawing.Point(73, 393);
+            this.BtnIncrease.Name = "BtnIncrease";
+            this.BtnIncrease.Size = new System.Drawing.Size(186, 23);
+            this.BtnIncrease.TabIndex = 2;
+            this.BtnIncrease.Text = "Увеличить скорость";
+            this.BtnIncrease.UseVisualStyleBackColor = true;
+            // 
+            // BtnDecrease
+            // 
+            this.BtnDecrease.Location = new System.Drawing.Point(563, 393);
+            this.BtnDecrease.Name = "BtnDecrease";
+            this.BtnDecrease.Size = new System.Drawing.Size(186, 23);
+            this.BtnDecrease.TabIndex = 3;
+            this.BtnDecrease.Text = "Уменьшить скорость";
+            this.BtnDecrease.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(365, 422);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Вращение";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(573, 423);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.BtnDecrease);
+            this.Controls.Add(this.BtnIncrease);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnIncrease;
+        private System.Windows.Forms.Button BtnDecrease;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Новая папка/3LabInd/Form2.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3LabInd
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        double k, l;
+        double[,] tr = new double[3, 3]; // матрица треугольника
+        double[,] pt = new double[5, 3]; // матрица пятиугольника
+        private float rotationAngleTr = 0;
+        private float rotationAnglePt = 0;
+        private float rotationSpeedTr = 1; // скорость вращения треугольника
+        private float rotationSpeedPt = 1; // скорость вращения пятиугольника
+
+        private void Init_tr()
+        {
+            // Координаты треугольника (центр в начале координат)
+            tr[0, 0] = -50; tr[0, 1] = 50; tr[0, 2] = 1;  // Вершина 1
+            tr[1, 0] = 50; tr[1, 1] = 50; tr[1, 2] = 1;   // Вершина 2
+            tr[2, 0] = 0; tr[2, 1] = -50; tr[2, 2] = 1;    // Вершина 3
+        }
+
+        private void Init_pt()
+        {
+            // Координаты пятиугольника (центр в начале координат)
+            pt[0, 0] = -50; pt[0, 1] = -20; pt[0, 2] = 1;
+            pt[1, 0] = 50; pt[1, 1] = -20; pt[1, 2] = 1;
+            pt[2, 0] = 70; pt[2, 1] = 30; pt[2, 2] = 1;
+            pt[3, 0] = 0; pt[3, 1] = 50; pt[3, 2] = 1;
+            pt[4, 0] = -70; pt[4, 1] = 30; pt[4, 2] = 1;
+        }
+
+        private void Draw_Tr()
+        {
+            k = pictureBox1.Width / 2;  // Центр по X
+            l = pictureBox1.Height / 2; // Центр по Y
+
+            Init_tr();
+            double[,] tr1 = Multiply_matr(tr, Init_matr_preob(k, l, rotationAngleTr));
+
+            Pen myPen = new Pen(Color.Blue, 2);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.DrawLine(myPen, (float)tr1[0, 0], (float)tr1[0, 1], (float)tr1[1, 0], (float)tr1[1, 1]);
+            g.DrawLine(myPen, (float)tr1[1, 0], (float)tr1[1, 1], (float)tr1[2, 0], (float)tr1[2, 1]);
+            g.DrawLine(myPen, (float)tr1[2, 0], (float)tr1[2, 1], (float)tr1[0, 0], (float)tr1[0, 1]);
+            g.Dispose();
+            myPen.Dispose();
+        }
+
+        private void Draw_Pt()
+        {
+            k = pictureBox1.Width / 4;  // Центр по X (тот же, что и для треугольника)
+            l = pictureBox1.Height / 2; // Центр по Y (тот же, что и для треугольника)
+
+            Init_pt();
+            double[,] pt1 = Multiply_matr(pt, Init_matr_preob(k, l, rotationAnglePt));
+
+            Pen myPen = new Pen(Color.Red, 2);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.DrawLine(myPen, (float)pt1[0, 0], (float)pt1[0, 1], (float)pt1[1, 0], (float)pt1[1, 1]);
+            g.DrawLine(myPen, (float)pt1[1, 0], (float)pt1[1, 1], (float)pt1[2, 0], (float)pt1[2, 1]);
+            g.DrawLine(myPen, (float)pt1[2, 0], (float)pt1[2, 1], (float)pt1[3, 0], (float)pt1[3, 1]);
+            g.DrawLine(myPen, (float)pt1[3, 0], (float)pt1[3, 1], (float)pt1[4, 0], (float)pt1[4, 1]);
+            g.DrawLine(myPen, (float)pt1[4, 0], (float)pt1[4, 1], (float)pt1[0, 0], (float)pt1[0, 1]);
+            g.Dispose();
+            myPen.Dispose();
+        }
+
+        private double[,] Init_matr_preob(double k1, double l1, double angle)
+        {
+            double angleRadians = angle * Math.PI / 180.0;
+            double cosTheta = Math.Cos(angleRadians);
+            double sinTheta = Math.Sin(angleRadians);
+
+            double[,] matr = new double[3, 3];
+            matr[0, 0] = cosTheta; matr[0, 1] = -sinTheta; matr[0, 2] = 0;
+            matr[1, 0] = sinTheta; matr[1, 1] = cosTheta; matr[1, 2] = 0;
+            matr[2, 0] = k1; matr[2, 1] = l1; matr[2, 2] = 1;
+
+            return matr;
+        }
+
+        private void picture_clean()
+        {
+            pictureBox1.Image = null;
+            pictureBox1.Refresh();
+        }
+
+        private double[,] Multiply_matr(double[,] a, double[,] b)
+        {
+            int n = a.GetLength(0);
+            int m = a.GetLength(1);
+            double[,] r = new double[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    r[i, j] = 0;
+                    for (int ii = 0; ii < m; ii++)
+                    {
+                        r[i, j] += a[i, ii] * b[ii, j];
+                    }
+                }
+            }
+            return r;
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            picture_clean();
+            Draw_Tr();
+            Draw_Pt();
+            rotationAngleTr -= rotationSpeedTr;
+            rotationAnglePt += rotationSpeedPt;
+
+            if (rotationAngleTr < -360) rotationAngleTr += 360;
+            if (rotationAnglePt > 360) rotationAnglePt -= 360;
+
+            await Task.Delay(10);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Up:
+                    rotationSpeedTr += 1;
+                    break;
+                case Keys.Down:
+                    rotationSpeedTr = Math.Max(1, rotationSpeedTr - 1);
+                    break;
+                case Keys.W:
+                    rotationSpeedPt += 1;
+                    break;
+                case Keys.S:
+                    rotationSpeedPt = Math.Max(1, rotationSpeedPt - 1);
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 10;
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            picture_clean();
+            Draw_Tr();
+            Draw_Pt();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Draw_Pt();
+            Draw_Tr();
+        }
+        //очистка бокса
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1 = null;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                rotationAngleTr = 0;
+                rotationAnglePt = 0;
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+    }
+}
